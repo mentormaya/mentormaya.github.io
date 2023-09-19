b0f1628f0e9e9c5ddd4613d6b3808140c4255d5a
Ajay Singh <ajay@mentormaya.com>
2023-09-19 13:08:00 +0545

    layout init

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import '@/public/css/globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,7 +13,7 @@ export default function MainLayout({
             <body className={`min-h-full`}>
                 <header className="flex justify-center content-center">
                     <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
-                        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+                        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:p-4">
                             <code className="font-mono font-bold">Er. </code>
                             Ajay Singh
                         </p>
