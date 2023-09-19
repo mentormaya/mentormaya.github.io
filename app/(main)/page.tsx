2913dc0b52bebdf54f86d473ac5b499fd090b33c
Ajay Singh <ajay@mentormaya.com>
2023-09-19 12:52:23 +0545

    layout and folder for root created

---
+++ app/(main)/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <main className="flex flex-col items-center justify-between p-24">
+        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+          this is the test content for the website and more is comming soon.
+        </div>
+      </main>
+    </>
+  )
+}
