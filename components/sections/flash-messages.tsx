9784412c28bf5726cf6c334611e4205e269e9dd9
Ajay Singh <ajay@mentormaya.com>
2023-10-05 17:28:14 +0545

    flash-news slider dummy posts added

--- components/sections/flash-messages.tsx
+++ components/sections/flash-messages.tsx
@@ -1,5 +1,15 @@
 import React from "react";
 
+const posts = [
+  "This is the first Post",
+  "This is the second Post",
+  "This is the third Post",
+  "This is the fourth Post",
+  "This is the fifth Post",
+  "This is the sixth Post",
+  "This is the seventh Post",
+];
+
 function FlashMessages() {
   return (
     <div className="">
