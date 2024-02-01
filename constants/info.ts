ad37aef57cf8c4729f5c227805eb26bebd5e1e81
Ajay Singh <admin@ajaysingh.com.np>
2024-02-01 20:01:42 +0545

    personal info updated

--- constants/info.ts
+++ constants/info.ts
@@ -3,12 +3,12 @@ export const info = {
   name: "Ajay Singh",
   email: "admin@ajaysingh.com.np",
   phone: "+977 9846449912",
-  address: "Kataiya Chauri, Janakpurdham, Madhesh, Nepal",
-  map: "https://www.google.com/maps/@26.7181412,85.9205459,18z?entry=ttu",
-  facebook: "https://facebook.com/ajay.mentormaya",
-  instagram: "https://instagram.com/mentormaya",
-  linkedin: "https://facebook.com/ajay.mentormaya",
-  twitter: "https://twitter.com/singh_ajaykumar",
+  address: "Sankhamul, Kathmandu, Bagmati, Nepal",
+  map: "https://www.google.com/maps/@26.7181412,85.9205459,18z?entry=ttu", // TODO update the map
+  facebook: "https://facebook.com/ajaykumarsingh.np",
+  instagram: "https://instagram.com/ajaykumarsingh.np",
+  linkedin: "https://www.linkedin.com/in/ajaykumarsingh/",
+  twitter: "https://twitter.com/ajay_singh_np",
   viber: "https://viber.me/9803522141",
   whatsapp: "https://wa.me/9803522141",
 };
