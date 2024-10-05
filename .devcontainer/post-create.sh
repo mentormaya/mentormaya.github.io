614055fa2caa611095b15d484fa6bc26fe04c5c5
Ajay Singh <admin@ajaysingh.com.np>
2024-10-05 23:46:30 +0545

    ctrl to lf

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 # Loading common functions
-. .devcontainer/utils.sh
+# Function to show banner messages
+banner() {
+    echo "----------------------------------------------------------------------------"
+    echo "$1"
+    echo "----------------------------------------------------------------------------"
+}
+
+# Function to verify execution of last command
+verifyln() {
+    if [ $? -ne 0 ]; then
+        echo "$1";
+        exit 1;
+    fi
+}
+
 banner "Running Post Create Script"
 
 # Install the version of Bundler specified in the Gemfile.lock
